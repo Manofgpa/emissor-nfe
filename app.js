69947739c3c7b704ab7ff32c825899bad435f137
Felipe Mano <felipe.mano@al.infnet.edu.br>
2021-07-26 20:35:00 -0300

    Add emissor

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 import Express from "express"
 import Path from "path"
+import states from "./views/loaders/states.js"
+
 
 const app = Express()
 const port = 5000
 const __dirname = Path.resolve()
+
+app.use(Express.urlencoded({ extended: false }))
 
 
 app.set('view engine', 'ejs')
@@ -18,8 +22,9 @@ app.get('/about', async (req, res) => {
     res.render('about')
 })
 
+
 app.get('/emissor', async (req, res) => {
-    res.render('generator')
+    res.render('generator', { states })
 })
 
 app.listen(port, () => {
