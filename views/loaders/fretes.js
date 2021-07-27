411ca20ae97f35a9ae46435fc7a59cf6839a50d4
Felipe Guedes Pereira de Azevedo Mano <felipe.mano@al.infnet.edu.br>
2021-07-27 19:54:45 -0300

    Add camelo

--- views/loaders/fretes.js
+++ views/loaders/fretes.js
@@ -2,7 +2,7 @@ const fretes = [
     { value: null, name: "Selecione" },
     { value: 0, name: "Correios" },
     { value: 1, name: "Transportadora" },
-    { value: 2, name: "Próprio Cliente" },
+    { value: 2, name: "Mão Própria" },
     { value: 3, name: "Camelo" },
     { value: 4, name: "Jadlog" }
 ]
