3bce7d39c50a0574b5a1099e91db9b81a81cb40d
Felipe Guedes Pereira de Azevedo Mano <felipe.mano@al.infnet.edu.br>
2021-07-28 18:26:34 -0300

    Danfe html & pdf viewer

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.get('/', async (req, res) => {
     res.render('pages/home')
 })
 
+
 app.get('/about', async (req, res) => {
     res.render('pages/about')
 })
@@ -33,36 +34,45 @@ app.get('/emissor', async (req, res) => {
     res.render('pages/generator', { states, modalidades, fretes })
 })
 
-app.get('/ejs', async (req, res) => {
-    res.render('pages/ex')
-})
 
-// app.post('/nfe', async (req, res) => {
-//     res.render('nfe')
-// })
-
-
-// TypeError: this.templateText.replace is not a function
-//     at Template.generateSource (/Users/manofgpa/Documents/projects/infnet/projects/projeto-1-emissor-nfe/node_modules/ejs/lib/ejs.js:721:25)
-//     at Template.compile (/Users/manofgpa/Documents/projects/infnet/projects/projeto-1-emissor-nfe/node_modules/ejs/lib/ejs.js:585:12)
-//     at Object.compile (/Users/manofgpa/Documents/projects/infnet/projects/projeto-1-emissor-nfe/node_modules/ejs/lib/ejs.js:396:16)
-//     at handleCache (/Users/manofgpa/Documents/projects/infnet/projects/projeto-1-emissor-nfe/node_modules/ejs/lib/ejs.js:233:18)
-//     at Object.exports.render (/Users/manofgpa/Documents/projects/infnet/projects/projeto-1-emissor-nfe/node_modules/ejs/lib/ejs.js:423:10)
-//     at file:///Users/manofgpa/Documents/projects/infnet/projects/projeto-1-emissor-nfe/app.js:47:21
-//     at Layer.handle [as handle_request] (/Users/manofgpa/Documents/projects/infnet/projects/projeto-1-emissor-nfe/node_modules/express/lib/router/layer.js:95:5)
-//     at next (/Users/manofgpa/Documents/projects/infnet/projects/projeto-1-emissor-nfe/node_modules/express/lib/router/route.js:137:13)
-//     at Route.dispatch (/Users/manofgpa/Documents/projects/infnet/projects/projeto-1-emissor-nfe/node_modules/express/lib/router/route.js:112:3)
-//     at Layer.handle [as handle_request] (/Users/manofgpa/Documents/projects/infnet/projects/projeto-1-emissor-nfe/node_modules/express/lib/router/layer.js:95:5)
-
-app.post("/nfe", (req, res, next) => {
+app.post("/nfe", async (req, res, next) => {
 
     const obj = { ...res.body }
-    
-    const html = fs.readFileSync('./views/pages/nfe.ejs', 'utf-8')
+
+    const html = fs.readFileSync('./views/pages/nfteste.ejs', 'utf-8')
     const nfe = ejs.render(html, obj)
     console.log(html);
     console.log(req.body)
 
+    let options = {
+        "height": "11.25in",
+        "width": "8.5in",
+        "header": {
+            "height": "0mm"
+        },
+        "footer": {
+            "height": "0mm",
+        }
+    }
+
+    pdf.create(nfe, options).toBuffer((err, data) => {
+        if (err) {
+            res.send(err)
+        } else {
+            res.contentType('application/pdf').send(data)
+        }
+    })
+
+})
+
+app.get('/nfteste', (req, res) => {
+    res.render('pages/nfteste')
+})
+
+app.get('/teste', (req, res) => {
+    const obj = { ...res.body }
+    const html = fs.readFileSync('./views/pages/nfteste.ejs', 'utf-8')
+    const nfe = ejs.render(html, obj)
     let options = {
         "height": "11.25in",
         "width": "8.5in",
@@ -81,7 +91,6 @@ app.post("/nfe", (req, res, next) => {
             res.contentType('application/pdf').send(data)
         }
     })
-
 })
 
 
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,13 +1,29 @@
 
 
 const addProduct = (id) => {
-    var x = document.getElementById(id)  
+    var x = document.getElementById(id)
     var node = x.rows[0].cloneNode(true)
     x.appendChild(node)
 }
 
 const removeProduct = (id) => document.getElementById(id).deleteRow(1)
 
-const updateTotal = () => {
+// update sub total price inputs values
+const updateSubTotal = () => {
 
+    const totalProd = document.getElementsByName('produtos_total')
+    const quantity = document.getElementsByName('produtos_quantidade')
+    const price = document.getElementsByName('produtos_preco')
+    totalProd.forEach((value, i) => {
+        totalProd[i].value = (Number(quantity[i].value) * Number(price[i].value)).toFixed(2)
+    })
+}
+
+const updateTotal = () => {
+    const totalProd = document.getElementsByName('produtos_total')
+    const totalNf = document.getElementById('produtos_total_nota')
+    totalNf.value = 0
+    totalProd.forEach((value, i) => {
+        totalNf.value = (Number(totalNf.value) + Number(totalProd[i].value)).toFixed(2)
+    })
 }
