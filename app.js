f1c868763e7552814239b5b6211e3f692f3201c6
Felipe Guedes Pereira de Azevedo Mano <felipe.mano@al.infnet.edu.br>
2021-07-28 07:31:26 -0300

    Add html-pdft

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import Path from "path"
 import states from "./views/loaders/states.js"
 import modalidades from "./views/loaders/modalidades.js"
 import fretes from "./views/loaders/fretes.js"
+import pdf from "html-pdf"
+import ejs from 'ejs'
 
 
 const app = Express()
@@ -32,6 +34,41 @@ app.get('/emissor', async (req, res) => {
 app.get('/ejs', async (req, res) => {
     res.render('ex')
 })
+
+// app.post('/nfe', async (req, res) => {
+//     res.render('nfe')
+// })
+
+app.get("/nfe", (req, res) => {
+    ejs.renderFile(Path.join(__dirname, './views/', "nfe.ejs"), (err, data) => {
+        if (err) {
+            res.send(err);
+        } else {
+            let options = {
+                "height": "11.25in",
+                "width": "8.5in",
+                "header": {
+                    "height": "20mm"
+                },
+                "footer": {
+                    "height": "20mm",
+                },
+            }
+            pdf.create(data, options).toFile("danfe.pdf", function (err, data) {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send("Nota emitida com sucesso");
+                }
+            })
+        }
+    })
+})
+
+
+
+
+
 
 app.listen(port, () => {
     console.log("Servidor rodando na porta", port)
