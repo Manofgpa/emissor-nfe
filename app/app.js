6e3f8be5564114aa9826044f7026598b372ecb5e
Felipe Guedes Pereira de Azevedo Mano <felipe.mano@al.infnet.edu.br>
2021-08-04 10:14:49 -0300

    Routes Created

--- app/app.js
+++ app/app.js
@@ -1,67 +1,21 @@
-import Path from "path"
-import Express from "express"
-import states from "./views/loaders/states.js"
-import modalidades from "./views/loaders/modalidades.js"
-import fretes from "./views/loaders/fretes.js"
-import paymentMethods from './views/loaders/meioPagamentos.js'
-import formaPagamentos from './views/loaders/formaPagamentos.js'
-import pdf from "html-pdf"
-import ejs from 'ejs'
-import fs from 'fs'
-import routes from './routes/routes.js'
+import path from "path"
+import express from "express"
+
 import Joi from "joi"
+import routes from "./routes/routes.js"
 
 
-const app = Express()
+const app = express()
 const port = 5000
-const __dirname = Path.resolve()
-
-routes(app)
+const __dirname = path.resolve()
 
 app.set('view engine', 'ejs')
-app.set('views', Path.join(__dirname + '/app', 'views'))
-app.use(Express.static(Path.join(__dirname + '/app', '/public')))
-app.use(Express.urlencoded({ extended: true }))
-// app.use(Express.json())
+app.set('views', path.join(__dirname + '/app', 'views'))
+app.use(express.static(path.join(__dirname + '/app', '/public')))
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', async (req, res) => {
-    res.render('pages/home')
-})
-
-app.get('/about', async (req, res) => {
-    res.render('pages/about')
-})
-
-app.get('/emissor', async (req, res) => {
-    res.render('pages/generator', { states, modalidades, fretes, paymentMethods, formaPagamentos })
-})
-
-app.post("/nfe", async (req, res, next) => {
-
-    const data = req.body
-    console.log(data)
-    const html = fs.readFileSync(__dirname + '/app/views/pages/nf.ejs', 'utf8')
-    const nfe = ejs.render(html, data)
-
-    let options = {
-        "height": "11.25in",
-        "width": "8.5in",
-        "header": {
-            "height": "0mm"
-        },
-        "footer": {
-            "height": "0mm",
-        }
-    }
-
-    pdf.create(nfe, options).toBuffer((err, data) => {
-        if (err) {
-            res.send(err)
-        } else {
-            res.contentType('application/pdf').send(data)
-        }
-    })
-})
+// Routes
+routes(app)
 
 app.listen(port, () => {
     console.log("Servidor rodando na porta", port)
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,10 +1,21 @@
-import Express from "express"
+import express from "express"
+import homeRoutes from './v1/homeRoutes.js'
+import aboutRoutes from './v1/aboutRoutes.js'
+import generatorRoutes from './v1/generatorRoutes.js'
+import nfeRoutes from './v1/nfeRoutes.js'
 
 
-export default (app) => {
 
-    const routev1 = Express.Router()
+export default app => {
 
-    app.use('v1' ,routev1)
+    const v1Route = express.Router()
+
+    homeRoutes(v1Route)
+    aboutRoutes(v1Route)
+    generatorRoutes(v1Route)
+    nfeRoutes(v1Route)
+
+    app.use('', v1Route)
 
 }
+
